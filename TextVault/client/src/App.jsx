f28ad3115b01fc430e136da2852fac6ec9ec2711
Mohammad Sameer <ms4492222@gmail.com>
2024-10-03 15:26:35 +0530

    added react-router-dom extension

---
+++ TextVault/client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Home from './pages/Home'
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+const App = () => {
+  return (
+    <div>
+   <Router>
+     <Routes>
+       <Route path="/" element={<Home />} />
+     </Routes>
+   </Router>
+    </div>
+  )
+}
+
+export default App
